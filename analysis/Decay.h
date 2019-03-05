5f618e744e08945cc00e0b5d2d606be10ee8da36
Jonathan Guiang <jkguiang@gmail.com>
2019-03-04 18:03:51 -0800

    adding modified decay header

--- analysis/Decay.h
+++ analysis/Decay.h
@@ -9,120 +9,110 @@
 using namespace std;
 using namespace tas;
 
-struct System {
-    // Member Variables
+struct Products {
+    // --> Member Variables <--
+    // Mother identification (for tag)
     int motherID;
+    // Products identification
     vector<int> daughterIDs;
+    vector<int> daughterIdxs;
+    // Physical properties
     LorentzVector p4;
     // Overload Constructor
-    System(int);
+    Products(int);
     // Destructor
-    ~System();
+    ~Products();
     // Methods
-    void Add(int, LorentzVector);
-    double Mass();
+    void Add(int, int, LorentzVector);
+    float Mass();
     string Tag();
 };
 
-System::System(int new_motherID) {
+Products::Products(int new_motherID) {
     motherID = new_motherID;
 }
 
-System::~System() {}
+Products::~Products() {}
 
-void System::Add(int new_daughterID, LorentzVector new_p4) {
+void Products::Add(int new_daughterID, int new_daughterIdx, LorentzVector new_p4) {
     p4 += new_p4;
     daughterIDs.push_back(new_daughterID);
+    daughterIdxs.push_back(new_daughterIdx);
     return;
 }
 
-double System::Mass() {
+float Products::Mass() {
     return p4.M();
 }
 
-string System::Tag() {
-    if (daughterIDs.size() == 0) return to_string(motherID)+"EMPTY";
-    int daughterSum = 0;
+string Products::Tag() {
+    if (daughterIDs.size() == 0) return to_string(motherID)+"_EMPTY";
+    int sum = 0;
     for (unsigned int i = 0; i < daughterIDs.size(); i++) {
-        daughterSum += daughterIDs.at(i);
+        sum += daughterIDs.at(i);
     }
-    return to_string(motherID)+"_"+to_string(daughterSum);
+    return to_string(motherID)+"_"+to_string(sum);
 }
 
 struct Decay {
-    // Member Variables
+    // --> Member Variables <--
+    // Decay identification
     int motherID;
+    int motherIdx;
     vector<int> daughterIDs;
-    vector<System*> systems;
+    Products* products;
     string tag;
 
     // Overload Constructor
-    Decay(int, vector<int>);
+    Decay(int, vector<int>, int);
     // Destructor
     ~Decay();
     // Methods
-    void NewSystem();
-    vector<System*> GetSystems();
-    System* GetSystem(int);
-    void Add(int, LorentzVector, int);
-    void Pop();
-    string MakeTag();
-    bool Full(int);
+    void SetMotherIdx(int);
+    string Tag();
+    void Reset();
+    void Add(int, int, LorentzVector);
+    bool Full();
 };
 
-Decay::Decay(int new_motherID, vector<int> new_daughterIDs) {
+Decay::Decay(int new_motherID, vector<int> new_daughterIDs, int new_motherIdx = -1) {
+    // Save mother/daughter PDG IDs
     motherID = new_motherID;
+    motherIdx = new_motherIdx;
     daughterIDs = new_daughterIDs;
-    tag = MakeTag();
+    // Make unique tag for system
+    tag = Tag();
+    // Initialize products
+    products = new Products(new_motherID);
 }
 
 Decay::~Decay() {}
 
-string Decay::MakeTag() {
-    if (daughterIDs.size() == 0) return to_string(motherID)+"EMPTY";
-    int daughterSum = 0;
+void Decay::SetMotherIdx(int new_motherIdx) {
+    motherIdx = new_motherIdx;
+    return;
+}
+
+string Decay::Tag() {
+    if (daughterIDs.size() == 0) return to_string(motherID)+"_EMPTY";
+    int sum = 0;
     for (unsigned int i = 0; i < daughterIDs.size(); i++) {
-        daughterSum += daughterIDs.at(i);
+        sum += daughterIDs.at(i);
     }
-    return to_string(motherID)+"_"+to_string(daughterSum);
+    return to_string(motherID)+"_"+to_string(sum);
 }
 
-void Decay::NewSystem() {
-    System* sys = new System(motherID);
-    systems.push_back(sys);
+void Decay::Reset() {
+    delete products;
+    products  = new Products(motherID);
     return;
 }
 
-vector<System*> Decay::GetSystems() {
-    return systems;
-}
-
-System* Decay::GetSystem(int index = -1) {
-    if (index == -1) {
-        index = systems.size()-1;
-    }
-    return systems.at(index);
-}
-
-void Decay::Add(int new_daughterID, LorentzVector new_p4, int index = -1) {
-    if (index == -1) {
-        index = systems.size()-1;
-    }
-    System* sys = systems.at(index);
-    sys->Add(new_daughterID, new_p4);
+void Decay::Add(int new_daughterID, int new_daughterIdx, LorentzVector new_p4) {
+    products->Add(new_daughterID, new_daughterIdx, new_p4);
     return;
 }
 
-void Decay::Pop() {
-    System* sys = systems.at(systems.size()-1);
-    systems.pop_back();
-    delete sys;
-    return;
-}
-
-bool Decay::Full(int index = -1) {
-    if (index == -1) {
-        index = systems.size()-1;
-    }
-    return (systems.at(index)->Tag() == tag);
+bool Decay::Full() {
+    return (products->Tag() == tag);
 }
