8c5e1a273dcdcadccdaac817725b4a3a4294dc2c
Jonathan Guiang <jkguiang@gmail.com>
2019-04-03 16:10:10 -0700

    added 'doAll-style' macro for running script for future reference

---
+++ analysis/runBuildMCTree.C
@@ -0,0 +1,13 @@
+{
+  gSystem->Load("/home/users/jguiang/projects/mt2/MT2Analysis/CORE/CMS3_CORE.so");
+
+  gROOT->ProcessLine(".L mcTree.C+");
+  gROOT->ProcessLine(".L BuildMCTree.C+");
+
+  // MONTE CARLO
+  TChain *priv_mc = new TChain("Events"); 
+  priv_mc->Add("/hadoop/cms/store/user/bemarsh/ProjectMetis/WH_HtoRhoGammaPhiGamma_privateMC_102x_MINIAOD_v1/merged_ntuple_1.root");
+  BuildMCTree(priv_mc, "mcTree.root");
+  // END MONTE CARLO
+
+}
