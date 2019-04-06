0f30d69b9757975b2cf12ca14c010a0b473f16ea
Jonathan Guiang <jkguiang@gmail.com>
2019-04-06 12:39:02 -0700

    updated ntuple location

--- analysis/runBuildMCTree.C
+++ analysis/runBuildMCTree.C
@@ -6,7 +6,7 @@
 
   // MONTE CARLO
   TChain *priv_mc = new TChain("Events"); 
-  priv_mc->Add("/hadoop/cms/store/user/bemarsh/ProjectMetis/WH_HtoRhoGammaPhiGamma_privateMC_102x_MINIAOD_v1/merged_ntuple_1.root");
+  priv_mc->Add("/hadoop/cms/store/group/snt/run2_mc2018_private/WH_HtoRhoGammaPhiGamma_privateMC_102x_MINIAOD_v1/merged_ntuple_1.root");
   BuildMCTree(priv_mc, "mcTree.root");
   // END MONTE CARLO
 
