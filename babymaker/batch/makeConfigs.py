fd5332f73bc8a70367f06d1a13463ebefcbf69ac
Jonathan Guiang <jkguiang@gmail.com>
2019-04-23 11:26:53 -0700

    updated batch submission files

---
+++ babymaker/batch/makeConfigs.py
@@ -0,0 +1,73 @@
+import glob, os, subprocess
+from textwrap import dedent
+
+def MakeConfigs(sample, project, test=False):
+
+    # Check for valid sample path
+    if not os.path.isdir(sample):
+        print("WARNING: Skipping {} -- directory does not exist.".format(sample))
+        return
+
+    # Get sample files
+    files = glob.glob("{}/*.root".format(sample))
+    # Check for non-zero input .root files
+    if len(files) == 0:
+        print("WARNING: Skipping {} -- contains 0 .root files.".format(sample))
+        return
+
+    # Get sample name
+    if sample[-1] == "/":
+        sample = sample[:-1]
+    splitSample = (sample.split("/")[-1]).split("_")
+    name = splitSample[0]
+    if "Tune" not in splitSample[1]:
+        name += "_"+splitSample[1]
+
+    # Get output directory
+    user = os.environ["USER"]
+    outdir = "/hadoop/cms/store/user/{0}/{1}/{2}".format(user, project, name)
+
+    # Create config directory if missing
+    if not os.path.isdir("configs"): os.mkdir("configs")
+
+    # Write config file
+    with open("configs/{0}config_{1}.cmd".format("test_" if test else "", name), 'w') as fout:
+        # Get credentials
+        x509file = subprocess.check_output(["find","/tmp/", "-maxdepth", "1", "-type", "f", "-user", user, "-regex", "^.*x509.*$"])
+        os.system("mkdir -p /data/tmp/{0}/condor_job_logs/{1}/".format(user, project))
+        os.system("mkdir -p /data/tmp/{0}/condor_submit_logs/{1}/".format(user, project))
+        # Write config header
+        fout.write(dedent("""
+        universe=Vanilla
+        when_to_transfer_output = ON_EXIT
+        transfer_input_files=wrapper.sh, package.tar.xz
+        +DESIRED_Sites="T2_US_UCSD"
+        +remote_DESIRED_Sites="T2_US_UCSD"
+        +Owner = undefined
+        log=/data/tmp/{0}/condor_submit_logs/{1}/condor_12_01_16.log
+        output=/data/tmp/{0}/condor_job_logs/{1}/1e.$(Cluster).$(Process).out
+        error =/data/tmp/{0}/condor_job_logs/{1}/1e.$(Cluster).$(Process).err
+        notification=Never
+        x509userproxy={2}
+
+        """.format(user, project, x509file)))
+        # Write executable calls
+        jobid = 0
+        for f in files:
+            jobid = (f.split("_")[-1]).split(".")[0]
+            fout.write("executable=wrapper.sh\n")
+            fout.write("transfer_executable=True\n")
+            fout.write("arguments={0} {1} {2}\n".format(jobid, sample, outdir))
+            fout.write("queue\n\n")
+            if test: return
+
+    return
+
+if __name__ == "__main__":
+    project = "rare-higgs"
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04", project, test=True)
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/TTGamma_SingleLeptFromT_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04", project, test=True)
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/TTGamma_SingleLeptFromTbar_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04", project, test=True)
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2_MINIAODSIM_CMS4_V10-02-04", project, test=True)
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04", project, test=True)
+    MakeConfigs("/hadoop/cms/store/group/snt/run2_mc2018/TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04", project, test=True)
--- babymaker/batch/wrapper.sh
+++ babymaker/batch/wrapper.sh
@@ -1,92 +1,73 @@
 #!/bin/bash
 
-#
-# args
-#
-
+# --> Arguments <-- #
+# Input
 FILEID=$1
-TAG=$2
-STLTAG=$3
-COPYDIR=$4
-
-return
-
+SAMPLE=$2
+COPYDIR=$3
+# Log input arguments
 echo "[wrapper] FILEID    = " ${FILEID}
 echo "[wrapper] COPYDIR   = " ${COPYDIR}
 
-#
-# set up environment
-#
+# --> Environment <-- #
+# Version using cvmfs install of CMSSW
 CMSSW_VERSION=CMSSW_9_4_1
-
-###version using cvmfs install of CMSSW
 echo "[wrapper] setting env"
 export SCRAM_ARCH=slc6_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 OLDDIR=`pwd`
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
-#cmsenv
+# Set up cmssw env
 eval `scramv1 runtime -sh`
 cd $OLDDIR
-
-
+# Log basic job info
 echo "[wrapper] hostname  = " `hostname`
 echo "[wrapper] date      = " `date`
 echo "[wrapper] linux timestamp = " `date +%s`
 
-#
-# untar input sandbox
-#
-
+# --> Input Decompression <-- #
+# Log cwd
 echo "[wrapper] current directory is:"
 pwd
-
+# Un-tar input tarball
 echo "[wrapper] extracting input sandbox"
 TARFILE=`ls *.tar.xz`
 tar -xJf $TARFILE
-
-#source job_input/setupenv.sh
-#printenv
-
+# Log input contents
 echo "[wrapper] input contents are"
 ls -a
 
+# --> Main Process <-- #
+# Input file
+INFILE="${SAMPLE}/merged_ntuple_${FILEID}.root"
+echo "[wrapper] running: python doAll.py $INFILE" 
+# Run main process
+python doAll.py $INFILE
 
-#
-# run it
-#
-INFILE="/hadoop/cms/store/user/bemarsh/LGAD/traj_inputs_for_jonathan/${TAG}/output_${FILEID}.txt"
-echo "[wrapper] running: python btlsim.py" "/nfs-7/userdata/jguiang/rare-higgs/stl/${STLTAG}" $INFILE "output.root" 
-
-python doAll.py
-
-#
-# do something with output
-#
-
+# --> Output <-- #
+# Log output
 echo "[wrapper] output is"
 ls -lh
-
-
+# Skim output
+echo "[wrapper] skimming output"
+python copyTree.py output.root skimmed.root -1 0 "tree" "recoMeson_nCands > 0"
+# Mark output to be copied
 echo "[wrapper] copying file"
-OUTPUT=output.root
+OUTPUT=skimmed.root
 echo "[wrapper] OUTPUT = " ${OUTPUT}
 
-#
-# clean up
-#
-
+# --> Clean-up <-- #
+# Set up destination directory
 if [ ! -d "${COPYDIR}" ]; then
     echo "creating output directory " ${COPYDIR}
     mkdir ${COPYDIR}
 fi
-
+# Copy output to destination
 export LD_PRELOAD=/usr/lib64/gfal2-plugins/libgfal_plugin_xrootd.so
 gfal-copy -p -f -t 4200 --verbose file://`pwd`/${OUTPUT} gsiftp://gftp.t2.ucsd.edu${COPYDIR}/output_${FILEID}.root
-
+# Clean up remaining files
 echo "[wrapper] cleaning up"
 for FILE in `find . -not -name "*stderr" -not -name "*stdout"`; do rm -rf $FILE; done
 echo "[wrapper] cleaned up"
 pwd
 ls
-#
