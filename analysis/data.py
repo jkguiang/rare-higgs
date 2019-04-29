a11ab15a86e5d5250b3c3f3d20f984006275a588
Jonathan Guiang <jkguiang@gmail.com>
2019-04-29 15:12:40 -0700

    now saves pkl files for faster future use

--- analysis/data.py
+++ analysis/data.py
@@ -1,21 +1,42 @@
 # Handling ROOT files
-import uproot, os
+import os
+import uproot, pandas as pd
 
-def GetData(inDir, verbose=False):
+def GetData(inDir, pklJar="pickles/", verbose=False):
     """ Return dictionary of Pandas dataframes """
-
+    # Check if directories exist
     if not os.path.isdir(inDir):
-        print("ERROR: {} does not exist.")
+        print("ERROR: {} does not exist.".format(inDir))
         return
-
+    if not os.path.isdir(pklJar):
+        os.mkdir(pklJar)
+    # Check naming convention
+    if inDir[-1] != "/":
+        inDir += "/"
+    if pklJar[-1] != "/":
+        pklJar += "/"
+    # Make dictionary of dataframes
     data = {}
     for output in os.listdir(inDir):
+        # Get file name
         name = (output.split("output_")[-1]).split(".")[0]
-        f=uproot.open("outputs/"+output) # TFile
-        t=f["tree"] # TTree
-        data[name] = t.pandas.df() # dataframe
+        # Pickle file
+        pkl = pklJar+name+".pkl"
+        pickled = os.path.isfile(pkl)
+        # Open file, save dataframe
+        if not pickled:
+            f=uproot.open(inDir+output) # TFile
+            t=f["tree"] # TTree
+            data[name] = t.pandas.df() # dataframe
+        else:
+            data[name] = pd.read_pickle(pkl)
+        # Pickle dataframe
+        if not pickled: data[name].to_pickle(pkl)
 
     if verbose:
         print("Loaded Dataframes:\n    "+"\n    ".join(data.keys()))
 
     return data
+
+if __name__ == "__main__":
+    GetData("outputs", verbose=True)
