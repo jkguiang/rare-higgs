a3677a2a64f3a07a39a4c9d93520c740f33000fa
Jonathan Guiang <jkguiang@gmail.com>
2019-04-30 11:25:08 -0700

    added Nick's write_json function

--- analysis/data.py
+++ analysis/data.py
@@ -1,6 +1,7 @@
+# General imports
+import os, ast, json
 # Handling ROOT files
-import os
-import uproot, pandas as pd
+import uproot, pandas as pd, numpy as np
 
 def GetData(inDir, pklJar="pickles/", verbose=False):
     """ Return dictionary of Pandas dataframes """
@@ -37,6 +38,67 @@ def GetData(inDir, pklJar="pickles/", verbose=False):
         print("Loaded Dataframes:\n    "+"\n    ".join(data.keys()))
 
     return data
+
+def WriteJSON(fname, bst, feature_names, class_labels=[]):
+    """ Return JSON of BDT (Written by Nick Amin) """
+    buff = "[\n"
+    trees = bst.get_dump()
+    ntrees = len(trees)
+    for itree,tree in enumerate(trees):
+        prev_depth = 0
+        depth = 0
+        for line in tree.splitlines():
+            depth = line.count("\t")
+            nascending = prev_depth - depth
+            (depth == prev_depth-1)
+            # print ascending, depth, prev_depth
+            prev_depth = depth
+            parts = line.strip().split()
+            padding = "    "*depth
+            for iasc in range(nascending):
+                buff += "{padding}]}},\n".format(padding="    "*(depth-iasc+1))
+            if len(parts) == 1:  # leaf
+                nodeid = int(parts[0].split(":")[0])
+                leaf = float(parts[0].split("=")[-1])
+                # print "leaf: ",depth,nodeid,val
+                buff += """{padding}{{ "nodeid": {nodeid}, "leaf": {leaf} }},\n""".format(
+                        padding=padding,
+                        nodeid=nodeid,
+                        leaf=leaf,
+                        )
+            else:
+                nodeid = int(parts[0].split(":")[0])
+                split, split_condition = parts[0].split(":")[1].replace("[","").replace("]","").split("<")
+                split_condition = float(split_condition)
+                yes, no, missing = map(lambda x:int(x.split("=")[-1]), parts[1].split(","))
+                buff += """{padding}{{ "nodeid": {nodeid}, "depth": {depth}, "split": "{split}", "split_condition": {split_condition}, "yes": {yes}, "no": {no}, "missing": {missing}, "children": [\n""".format(
+                        padding=padding,
+                        nodeid=nodeid,
+                        depth=depth,
+                        split=split,
+                        split_condition=split_condition,
+                        yes=yes,
+                        no=no,
+                        missing=missing,
+                        )
+        for i in range(depth):
+            padding = "    "*(max(depth-1,0))
+            if i == 0:
+                buff += "{padding}]}}".format(padding=padding)
+            else:
+                buff += "\n{padding}]}}".format(padding=padding)
+            depth -= 1
+        if itree != len(trees)-1:
+            buff += ",\n"
+    buff += "\n]"
+    # print buff
+    to_dump = {
+            "trees": list(ast.literal_eval(buff)),
+            "feature_names": feature_names,
+            "class_labels": map(int,np.array(class_labels).tolist()), # numpy array not json serializable
+            }
+    with open(fname, "w") as fhout:
+        json.dump(to_dump,fhout,indent=2)
 
 if __name__ == "__main__":
     GetData("outputs", verbose=True)
