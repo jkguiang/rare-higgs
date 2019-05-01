c69906d0e634fdf9fd8ae3df8cb005014d9f0704
Jonathan Guiang <jkguiang@gmail.com>
2019-05-01 14:10:46 -0700

    only plots phi or rho events

--- analysis/plot.py
+++ analysis/plot.py
@@ -9,12 +9,14 @@ from matplottery import Hist1D,Hist2D, plot_stack
 import matplotlib.pyplot as plt
 # Custom
 from data import GetData
+from config import config
 
 class Plots():
-    def __init__(self, signal, dataframes, outDir="plots/", lumi=137.0, verbose=False):
+    def __init__(self, config, data, outDir="plots/", lumi=137.0, verbose=False):
         # Args
-        self.signal = signal 
-        self.dataframes = dataframes
+        self.signal = config["signal"] 
+        self.analysis = config["analysis"]
+        self.data = data
         # Kwargs
         self.outDir = outDir
         self.lumi = lumi
@@ -24,7 +26,6 @@ class Plots():
         # Init functions
         self._initColors()
         self._initOutput()
-        self._initDataframes()
 
     def _initColors(self):
         """ Initialize available colors """
@@ -32,7 +33,7 @@ class Plots():
               [0.4, 0.6, 1.0], [0.4, 0.8, 0.4], [0.9, 0.9, 0.9],
               [0.4, 0.4, 0.6], [0.0, 0.4, 0.0]
              ]
-        for i, name in enumerate(dataframes.keys()):
+        for i, name in enumerate(self.data.keys()):
             color = [1.0,0.0,0.0] if name == self.signal else rgbs[i]
             self.colors[name] = color
 
@@ -44,30 +45,6 @@ class Plots():
         if not os.path.isdir(out): os.mkdir(out)
         if out[-1] != "/":
             self.outDir += "/"
-
-        return
-
-    def _initDataframes(self):
-        # Get list of sample names
-        samples = self.dataframes.keys()
-        # Move signal name to front
-        samples.insert(0, samples.pop(samples.index(signal)))
-        # Add bookkeeping columns, delete bad rows
-        for name, df in self.dataframes.iteritems():
-            # Add signal bool and dataset name columns
-            df["stype"] = samples.index(name)
-            df["signal"] = (df["stype"] == 0)
-            # Require found lepton, photon, meson
-            df = df.drop(df.query("recoWLepton_pt < 0 or recoPhi_pt < 0 or recoRho_pt < 0 or recoGamma_pt < 0").index)
-            # Avoid double-counting prompt photons
-            genGammaMatch = ( df.genRecoGamma_isMatch == 1 )
-            minGammaParton = ( df.minGammaParton_dR > 0.4 )
-            if name in ["WGToLNuG", "TTGamma_SingleLeptFromT", "TTGamma_SingleLeptFromTbar"]:
-                df = df.drop(df.query("not @genGammaMatch or not @minGammaParton").index)
-            elif name in ["WJetsToLNu", "TTJets_SingleLeptFromT", "TTJets_SingleLeptFromTbar"]:
-                df = df.drop(df.query("@genGammaMatch and @minGammaParton").index)
-            # Update dataframe in dictionary
-            self.dataframes[name] = df
 
         return
 
@@ -96,16 +73,21 @@ class Plots():
         """ Make plot of stacked 1D histograms """
         bgsPlots = []
         sigPlots = []
-        for name, df in dataframes.iteritems():
+        for name, df in self.data.iteritems():
+            # Drop rho or phi events
+            if self.analysis == "phi":
+                df = df.drop(df.query("genHiggsMeson_id == 113").index)
+            elif self.analysis == "rho":
+                df = df.drop(df.query("genKp_pt > 0").index)
             # Get data
-            data = df.loc[df[col] != -999, col].to_numpy()
+            x = df.loc[(df[col] != -999), col].to_numpy()
             # Get scale1fb
             weights = df.loc[(df.scale1fb != -999) & (df[col] != -999), "scale1fb"].to_numpy()
             # Manually set signal scalef1b
             if name == self.signal and np.array_equal(weights, np.ones_like(weights)):
                 weights = 0.0*weights+sigWeight
             # Plot data
-            thisPlot = Hist1D(data,label=name.split("_")[0], bins=np.linspace(xMin,xMax,nBins), weights=weights, color=self.colors[name])
+            thisPlot = Hist1D(x,label=name.split("_")[0], bins=np.linspace(xMin,xMax,nBins), weights=weights, color=self.colors[name])
             # Add plot to stack
             if name == self.signal: sigPlots.append(thisPlot)
             else: bgsPlots.append(thisPlot)
@@ -119,8 +101,7 @@ class Plots():
         return
 
 if __name__ == "__main__":
-    signal = "WH_HtoRhoGammaPhiGamma"
     print("Loading dataframes...")
     dataframes = GetData("outputs", verbose=True)
-    plots = Plots(signal, dataframes, verbose=True)
+    plots = Plots(config, dataframes, verbose=True)
     plots.Plot()
