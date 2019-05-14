fc2926ecb13c1a2e89300dbb709655960d39c68e
Jonathan Guiang <jkguiang@gmail.com>
2019-05-13 17:49:00 -0700

    fixed configs

--- babymaker/batch/configs.py
+++ babymaker/batch/configs.py
@@ -13,16 +13,16 @@ samples_2018 = {
         "EGamma_Run2018D-PromptReco-v2_MINIAOD_CMS4_V10-02-04"     : "data_Run2018D_EGamma_PromptReco-v2",
         },
     "ttbar" : {
-        "TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04"    : "ttsl_top",
-        "TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04" : "ttsl_tbar",
-        "TTGamma_SingleLeptFromT_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04" : "ttsl_top",
-        "TTGamma_SingleLeptFromTbar_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04": "ttsl_tbar",
+        "TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04"       : "ttsl_top2018",
+        "TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04"    : "ttsl_tbar2018",
+        "TTGamma_SingleLeptFromT_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04"    : "ttg_top2018",
+        "TTGamma_SingleLeptFromTbar_TuneCP5_13TeV_madgraph_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-04" : "ttg_tbar2018",
         },
     "wjets" : {
-        "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2_MINIAODSIM_CMS4_V10-02-04" : "wjets",
+        "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2_MINIAODSIM_CMS4_V10-02-04" : "wjets2018",
         },
     "wgamma" : {
-        "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04"   : "wgamma",
+        "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-04"   : "wgamma2018",
         },
     "whiggs" : {
         "WH_HtoRhoGammaPhiGamma_privateMC_102x_MINIAOD_v1"   : "whiggs",
@@ -43,16 +43,16 @@ samples_2017 = {
         "SingleElectron_Run2017F-31Mar2018-v1_MINIAOD_CMS4_V10-02-05" : "data_Run2017F_EGamma_31Mar2018",
         },
     "ttbar" : {
-        "TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"         : "ttsl_top",
-        "TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"      : "ttsl_tbar",
-        "TTGamma_SingleLeptFromT_TuneCP5_PSweights_13TeV_madgraph_pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05" : "ttsl_top",
-        "TTGamma_SingleLeptFromTbar_TuneCP5_PSweights_13TeV_madgraph_pythia8_RunIIFall17MiniAOD-PU2017_94X_mc2017_realistic_v11-v1_MINIAODSIM_CMS4_V10-02-05"          : "ttsl_tbar",
+        "TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"         : "ttsl_top2017",
+        "TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"      : "ttsl_tbar2017",
+        "TTGamma_SingleLeptFromT_TuneCP5_PSweights_13TeV_madgraph_pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05" : "ttg_top2017",
+        "TTGamma_SingleLeptFromTbar_TuneCP5_PSweights_13TeV_madgraph_pythia8_RunIIFall17MiniAOD-PU2017_94X_mc2017_realistic_v11-v1_MINIAODSIM_CMS4_V10-02-05"          : "ttg_tbar2017",
         },
     "wjets" : {
-        "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2_MINIAODSIM_CMS4_V10-02-05" : "wjets",
+        "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2_MINIAODSIM_CMS4_V10-02-05" : "wjets2017",
         },
     "wgamma" : {
-        "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"   : "wgamma",
+        "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V10-02-05"   : "wgamma2017",
         },
     }
 
@@ -61,10 +61,10 @@ samples_2016 = {
         "SingleMuon_Run2016B-17Jul2018_ver2-v1_MINIAOD_CMS4_V10-02-05"     : "data_Run2016B_SingleMuon_17Jul2018_ver2-v1",
         "SingleMuon_Run2016C-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016C_SingleMuon_17Jul2018",
         "SingleMuon_Run2016D-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016D_SingleMuon_17Jul2018",
-        "SingleMuon_Run2016E-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016C_SingleMuon_17Jul2018",
-        "SingleMuon_Run2016F-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016D_SingleMuon_17Jul2018",
-        "SingleMuon_Run2016G-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016C_SingleMuon_17Jul2018",
-        "SingleMuon_Run2016H-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016D_SingleMuon_17Jul2018",
+        "SingleMuon_Run2016E-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016E_SingleMuon_17Jul2018",
+        "SingleMuon_Run2016F-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016F_SingleMuon_17Jul2018",
+        "SingleMuon_Run2016G-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016G_SingleMuon_17Jul2018",
+        "SingleMuon_Run2016H-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"          : "data_Run2016H_SingleMuon_17Jul2018",
         "SingleElectron_Run2016B-17Jul2018_ver2-v1_MINIAOD_CMS4_V10-02-05" : "data_Run2016B_SingleElectron_17Jul2018_ver2-v1",
         "SingleElectron_Run2016C-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"      : "data_Run2016C_SingleElectron_17Jul2018-v1",
         "SingleElectron_Run2016D-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"      : "data_Run2016D_SingleElectron_17Jul2018-v1",
@@ -74,19 +74,19 @@ samples_2016 = {
         "SingleElectron_Run2016H-17Jul2018-v1_MINIAOD_CMS4_V10-02-05"      : "data_Run2016H_SingleElectron_17Jul2018-v1",
         },
     "ttbar" : {
-        "TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V09-04-17"         : "ttsl_top",
-        "TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2_MINIAODSIM_CMS4_V10-02-05"    : "ttsl_top_ext",
-        "TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V09-04-17"      : "ttsl_tbar",
-        "TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2_MINIAODSIM_CMS4_V10-02-05" : "ttsl_tbar_ext",
-        "TTGamma_SingleLeptFromT_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V10-02-05"         : "ttsl_top",
-        "TTGamma_SingleLeptFromTbar_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1_MINIAODSIM_CMS4_V10-02-05"      : "ttsl_tbar",
+        "TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V09-04-17"         : "ttsl_top2016",
+        "TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2_MINIAODSIM_CMS4_V10-02-05"    : "ttsl_top_ext2016",
+        "TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V09-04-17"      : "ttsl_tbar2016",
+        "TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2_MINIAODSIM_CMS4_V10-02-05" : "ttsl_tbar_ext2016",
+        "TTGamma_SingleLeptFromT_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2_MINIAODSIM_CMS4_V10-02-05"         : "ttg_top2016",
+        "TTGamma_SingleLeptFromTbar_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1_MINIAODSIM_CMS4_V10-02-05"      : "ttg_tbar2016",
         },
     "wjets" : {
-        "WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext2-v2_MINIAODSIM_CMS4_V10-02-05" : "wjets",
+        "WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext2-v2_MINIAODSIM_CMS4_V10-02-05" : "wjets2016",
         },
     "wgamma" : {
-        "WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v1_MINIAODSIM_CMS4_V10-02-05"   : "wgamma",
-        "WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext3-v1_MINIAODSIM_CMS4_V09-04-17"   : "wgamma_ext",
+        "WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v1_MINIAODSIM_CMS4_V10-02-05"   : "wgamma2016",
+        "WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext3-v1_MINIAODSIM_CMS4_V09-04-17"   : "wgamma_ext2016",
         },
     }
 
@@ -106,7 +106,7 @@ if __name__ == "__main__":
             thisBase = "{0}{1}2017{2}".format(base, "data" if typ == "data" else "mc", "/")
             for sample, name in samples.iteritems():
                 MakeConfigs("2017", thisBase+sample, project, name, test=test, tag=tag)
-        print("Making 2017{}config files...".format(" test " if test else " "))
+        print("Making 2016{}config files...".format(" test " if test else " "))
         for typ, samples in tqdm(list(samples_2016.iteritems())):
             thisBase = "{0}{1}2016{2}".format(base, "data" if typ == "data" else "mc", "_94x/")
             for sample, name in samples.iteritems():
